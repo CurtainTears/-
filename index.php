93562e383f943a612fee46fb6d7cef9f3cb1231a
Muhosia <curtaintears@gmail.com>
2025-03-22 22:48:09 +0800

    Update index.php

--- index.php
+++ index.php
@@ -11,4 +11,4 @@ header('Content-type: text/html; charset=utf-8');
  * window['setServiceListdUrl'] = "http://192.168.50.50:82/server";
  * start();
  */
-exit('{"login":[999,997,990],"serverlist":[{"name":"无天测试","serverlist":[{"originalSrvid":1,"srvid":1,"srvaddr":"121.37.155.68","id":2，"serverAlias":"s1","serverName":"国内1区","opentime":"2022-03-23 23:59:51","srvport":35317,"type":2,"pf":"yfbx"},{"originalSrvid":1,"srvid":1,"srvaddr":"103.213.4.14","id":3,"serverAlias":"s1","serverName":"国内1区","opentime":"2022-03-23 23:59:51","srvport":10317,"type":2,"pf":"yfbx"}]}]}');
+exit('{"login":[999,997,990],"serverlist":[{"name":"无天测试","serverlist":[{"originalSrvid":1,"srvid":1,"srvaddr":"121.37.155.68","id":2,"serverAlias":"s1","serverName":"国内1区","opentime":"2022-03-23 23:59:51","srvport":35317,"type":2,"pf":"yfbx"},{"originalSrvid":1,"srvid":1,"srvaddr":"103.213.4.14","id":3,"serverAlias":"s1","serverName":"国内1区","opentime":"2022-03-23 23:59:51","srvport":10317,"type":2,"pf":"yfbx"}]}]}');
